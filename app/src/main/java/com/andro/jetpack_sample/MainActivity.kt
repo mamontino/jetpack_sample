df07dba5ce6452d005c84f4340480d72238de5bd
mamontov.a <yattzy54@gmail.com>
2022-01-20 22:34:35 +0200

    First commit

--- app/src/main/java/com/andro/jetpack_sample/MainActivity.kt
+++ app/src/main/java/com/andro/jetpack_sample/MainActivity.kt
@@ -1,47 +1,120 @@
 package com.andro.jetpack_sample
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.andro.home.HomeFragment
-import com.andro.rules.RulesFragment
-import com.andro.jetpack_sample.databinding.ActivityMainBinding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.andro.domain.CountryModel
+import com.andro.home.CountryList
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            Column {
+                TopAppBar(
+                    title = {
+                        Text(text = getString(R.string.app_name))
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { }) {
+                            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Menu Btn")
+                        }
+                    },
+                    backgroundColor = Color.Transparent,
+                    contentColor = Color.Gray,
+                    elevation = 2.dp
+                )
 
-        setSupportActionBar(binding.toolbar)
+                TextInput()
 
-        binding.fab.setOnClickListener {
-            navigateToHome()
+                CountryList(
+                    list = getCountryList(),
+                    onSelectionChange = {})
+            }
+
         }
     }
 
-    private fun navigateToHome() {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, HomeFragment())
-            .commit()
+    private fun getCountryList(): List<CountryModel> {
+        val list = mutableListOf<CountryModel>()
+        for (i in 1..50) {
+            list.add(
+                CountryModel(
+                    "https://popperssu.ru/wp-content/uploads/Russia-Flag-PNG-Pic.png",
+                    "Country"
+                )
+            )
+        }
+        return list
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    @Composable
+    fun TextInput() {
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else                 -> super.onOptionsItemSelected(item)
+        val inputvalue = remember { mutableStateOf(TextFieldValue()) }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        )
+        {
+            TextField(
+                value = inputvalue.value,
+                onValueChange = { txt -> inputvalue.value = it },
+                placeholder = { Text(text = "Enter user name") },
+                modifier = Modifier.padding(all = 16.dp).fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.None,
+                    autoCorrect = true,
+                    keyboardType = KeyboardType.Text,
+                ),
+                textStyle = TextStyle(
+                    color = Color.Black,
+                    fontSize = 15.sp,
+                    fontFamily = FontFamily.SansSerif
+                ),
+                maxLines = 2,
+                activeColor = colorResource(id = R.color.purple_200),
+                singleLine = true,
+                inactiveColor = Color.Gray,
+                backgroundColor = Color.LightGray,
+                leadingIcon = {
+                    Icon(Icons.Filled.AccountCircle, tint = colorResource(id = R.color.purple_200))
+                },
+                trailingIcon = {
+                    Icon(Icons.Filled.Info, tint = colorResource(id = R.color.purple_200))
+                },
+            )
         }
     }
 }
+
--- app/src/main/java/com/andro/jetpack_sample/di/NetworkModule.kt
+++ app/src/main/java/com/andro/jetpack_sample/di/NetworkModule.kt
@@ -1,11 +1,8 @@
 package com.andro.jetpack_sample.di
 
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
